4edb5666889eda9070bde5c0a0ad9550b361a706
Matt Mireles <matt@mirel.es>
2012-11-16 12:40:56 -0800

    added Dan's open hour JS to html 2

--- Swig_web/html2/js/canOrder.js
+++ Swig_web/html2/js/canOrder.js
@@ -1,16 +1,16 @@
-<doc
-var now = new Date();
+<script type="text/javascript">
+	$(function() {
+	  $(“#order_btn”).click(function() {
+	    var now = new Date();
+	    var day = now.getDay();
+	    var hour = now.getUTCHours() - 8;
+	    if ((day >= 5 && (hour >= 12 || hour <= 2)) || (day === 0 && hour >=12)) {
+	      document.location='https://swig.wufoo.com/forms/order-now/';
+	    } else {
+	      document.location='https://swig.wufoo.com/forms/sorry-swig-is-currently-closed/';
+	    }
+	    return false;
+	  });
+	}
+</script>
 
-// 
-var day = now.getDay();
-
-var hour = now.getUTCHours() - 8;
-
-var canOrder = function(day, hour) {
-	if ((day >= 5 && (hour >= 12 || hour <= 2)) || (day === 0 && hour >=12) {
-alert("Sweet!!!! We're Open. ")
-	};
-	else {
-		alert("Bummer! We're closed for the night.")
-	};
-};
